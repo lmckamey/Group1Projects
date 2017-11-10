7406ad068b6fd76288470b9f5df945dbb08f160b
Colin Misbach <CMisbach@student.neumont.edu>
2017-11-09 23:07:59 -0700

    Basic Drawing Functionality Complete

--- WIPProject/WIPProject/UserControls/BasicDrawingControl.xaml.cs
+++ WIPProject/WIPProject/UserControls/BasicDrawingControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WIPProject.UserControls;
 
 namespace WIPProject.UserControls
 {
@@ -20,9 +21,106 @@ namespace WIPProject.UserControls
     /// </summary>
     public partial class BasicDrawingControl : UserControl
     {
+        Point mouseOldPosition;
+        Point mouseCurrentPosition;
+
+        private Line[] prevLines = new Line[100];
+        private int numOfLines = 0;
+
         public BasicDrawingControl()
         {
             InitializeComponent();
+
+            cnvDrawArea.Focus();
+            //AddHandler(Keyboard.KeyDownEvent, (KeyEventHandler)cnvDrawArea_KeyDown);
+
+            mouseOldPosition = new Point();
+            mouseCurrentPosition = new Point();
+        }
+
+        private void cnvDrawArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            mouseCurrentPosition = e.GetPosition(cnvDrawArea);
+
+            DrawLine(e);
+
+            mouseOldPosition = e.GetPosition(cnvDrawArea);
+        }
+
+        private void DrawLine(MouseEventArgs e)
+        {
+            if (e.LeftButton.Equals(MouseButtonState.Pressed))
+            {
+                Line l = new Line();
+
+                l.X1 = mouseOldPosition.X;
+                l.Y1 = mouseOldPosition.Y;
+                l.X2 = mouseCurrentPosition.X;
+                l.Y2 = mouseCurrentPosition.Y;
+
+                l.StrokeThickness = uscBrushSize.sldBrushSize.Value;
+                l.Stroke = uscColorPicker.elpCurrentColor.Fill.Clone();
+                l.Fill = l.Stroke;
+
+                l.StrokeEndLineCap = PenLineCap.Round;
+                l.StrokeStartLineCap = PenLineCap.Round;
+                l.StrokeLineJoin = PenLineJoin.Round;
+
+                cnvDrawArea.Children.Add(l);
+
+                if (numOfLines >= prevLines.Length)
+                    RemoveFirstLine();
+
+                prevLines[numOfLines++] = l;
+            }
+        }
+
+        private void cnvDrawArea_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            uscBrushSize.sldBrushSize.Value += e.Delta / 40;
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            cnvDrawArea.Children.Clear();
+        }
+
+        private void UndoLastLine()
+        {
+            if (numOfLines > 0)
+            {
+                Line lastLine = prevLines[numOfLines - 1];
+
+                cnvDrawArea.Children.Remove(lastLine);
+                prevLines[numOfLines - 1] = null;
+
+                --numOfLines;
+            }
+        }
+
+        private void RemoveFirstLine()
+        {
+            prevLines[0] = null;
+
+            for (int i = 0; i < prevLines.Length - 1; ++i)
+            {
+                Line top = prevLines[i + 1];
+                Line bottom = prevLines[i];
+
+                bottom = top;
+            }
+
+            --numOfLines;
+        }
+
+        private void cnvDrawArea_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+            if (true)
+            {
+                UndoLastLine();
+                cnvDrawArea.Background = new SolidColorBrush(Colors.Black);
+            }
         }
     }
 }
--- WIPProject/WIPProject/UserControls/ColorPickerControl.xaml.cs
+++ WIPProject/WIPProject/UserControls/ColorPickerControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WIPProject.Models;
 
 namespace WIPProject.UserControls
 {
@@ -23,6 +24,58 @@ namespace WIPProject.UserControls
         public ColorPickerControl()
         {
             InitializeComponent();
+
+            SliderColor sc = new SliderColor();
+            UpdateEllipse();
+
+            elpCurrentColor.DataContext = sc;
+            //Binding b = new Binding("ColorBrush");
+            //SetBinding(Ellipse.FillProperty, b);
+        }
+
+        private void sldRed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateEllipse(r: (int)((Slider)sender).Value);
+        }
+
+        private void sldGreen_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateEllipse(g: (int)((Slider)sender).Value);
+        }
+
+        private void sldBlue_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateEllipse(b: (int)((Slider)sender).Value);
+        }
+
+        private void sldAlpha_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateEllipse(a: (int)((Slider)sender).Value);
+        }
+
+        private void UpdateEllipse(int r = -1, int g = -1, int b = -1, int a = -1)
+        {
+            if (elpCurrentColor != null && elpCurrentColor.DataContext != null)
+                ((SliderColor)elpCurrentColor.DataContext).UpdateColors(r, g, b, a);
+        }
+
+        private Color GetEllipseColor()
+        {
+            Color c = new Color()
+            {
+                R = (byte)sldRed.Value,
+                G = (byte)sldGreen.Value,
+                B = (byte)sldBlue.Value,
+                A = (byte)sldAlpha.Value,
+            };
+
+            return c;
+        }
+
+        private void SetEllipseColor(Color color)
+        {
+            if (elpCurrentColor != null)
+                elpCurrentColor.Fill = new SolidColorBrush(color);
         }
     }
 }
