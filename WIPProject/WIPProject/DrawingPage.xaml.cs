08478d1deab778e7e9daefae1e6163c90bc20b3a
Colin Misbach <CMisbach@student.neumont.edu>
2017-11-09 17:23:13 -0700

    Initial Commit to Colin's Branch

---
+++ WIPProject/WIPProject/DrawingPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WIPProject
+{
+    /// <summary>
+    /// Interaction logic for DrawingPage.xaml
+    /// </summary>
+    public partial class DrawingPage : Window
+    {
+        public DrawingPage()
+        {
+            InitializeComponent();
+        }
+
+        private void txbChatBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.Equals(Key.Return))
+            {
+                lblTextWatermark.Visibility = Visibility.Visible;
+            }
+            else if (txbChatBox.GetLineText(0).Length >= 0 && !e.Key.Equals(Key.Back))
+            {
+                lblTextWatermark.Visibility = Visibility.Hidden;
+            }
+            else if (e.Key.Equals(Key.Back) && txbChatBox.GetLineText(0).Length <= 1)
+            {
+                lblTextWatermark.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
